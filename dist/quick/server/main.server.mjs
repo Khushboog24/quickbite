3127f97e6af148f42b3753f58198c6b91793641f
Khushboo <khushboogupta973@gmail.com>
2024-09-15 22:56:38 +0530

    updated the build file

--- dist/quick/server/main.server.mjs
+++ dist/quick/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-FUL47NZW.mjs";import"./chunk-BERSOZRK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RIPVU3F2.mjs";import"./chunk-GH5QXC53.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
